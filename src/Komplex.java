3e8c32174d49e9efcda3d6bb73c778d1bd859b03
L1nu5-21 <94972443+L1nu5-21@users.noreply.github.com>
2023-03-17 10:22:45 +0100

    Initial commit

---
+++ src/Komplex.java
@@ -0,0 +1,19 @@
+public class Komplex {
+    double a;
+    double b;
+    double imaginary;
+    double real;
+
+    public Komplex(double v, double v1) {
+    }
+
+    public Komplex add(Komplex b) {
+
+    }
+
+    public Komplex multiply(Komplex b) {
+    }
+
+    public double arg() {
+    }
+}
